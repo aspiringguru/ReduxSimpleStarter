bb05e8b0467308545fbf12d80f3f2243e3e03755
Matthew <bmatthwtaylor@gmail.com>
2018-06-21 16:26:20 +1000

    S1L19 start

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -21,10 +21,17 @@ class SearchBar extends Component {
     super(props);
     this.state = { term: ''};
   }
-  
+
   render() {
     //return <input onChange={this.onInputChange}  />;
-    return <input onChange={(event) => console.log(event.target.value) }  />;
+    //return <input onChange={(event) => console.log(event.target.value) }  />;
+    return (
+        <div>
+          <input onChange={(event) => this.setState({ term: event.target.value}) }  />;
+          Value of the input : {this.state.term}
+        </div>
+    );
+
   }
 
   /*
