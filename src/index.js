431d0ac6b2b5a621503b1ab01bbc0f02c42508ae
Matthew <bmatthwtaylor@gmail.com>
2018-06-21 18:01:47 +1000

    S1L23 start

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 
-//move this to import from a file excluded from the git repo
 const API_KEY = 'AIzaSyALMuCcKfk0855hdbTc7zcWDzOrNbHjV0s'
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
+class App extends Component {
 
-//create new component, this should produce html
-//const App = function(){
-//convert above to ES6 syntax.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = { videos: []  }
+    //YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+    //refactor above to this
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      console.log(videos);
+      //this.setState({ videos: videos});
+      //ES6 syntax allows us to compact above to next line when key & property have same name
+      this.setState({ videos });
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 //take this html and put it in the dom
 ReactDOM.render(<App />, document.querySelector('.container'));
