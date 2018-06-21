1c04ff52c05ebb70c434952cf9b1c7f15bb78296
Matthew <bmatthwtaylor@gmail.com>
2018-06-21 16:05:04 +1000

    S1L18 start

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -16,6 +16,12 @@ const SearchBar = () => {
 
 // refactor above as an ES6 class
 class SearchBar extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { term: ''};
+  }
+  
   render() {
     //return <input onChange={this.onInputChange}  />;
     return <input onChange={(event) => console.log(event.target.value) }  />;
