13435bb96ea93736f8d746acb7daa8ffb500ee9f
Matthew <bmatthwtaylor@gmail.com>
2018-06-22 13:46:05 +1000

    S1L27 start.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+//const VideoListItem = (props) => {
+//  const video = props.video;
+//new object to pull video from props object
+//next line is ES6 version of above 2 lines.
+const VideoListItem = ({video}) => {
+  console.log(video)
+  const imageUrl = video.snippet.thumbnails.default.url;
+  //get this tree from chrome > devtools > network > search > expand the tree
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src = {imageUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  )
 };
 
 export default VideoListItem;
