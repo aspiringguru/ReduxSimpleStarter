d3d2fae592f86998a888763d422d920e1a53683e
Matthew <bmatthwtaylor@gmail.com>
2018-06-23 12:58:03 +1000

    L29 completed and working. start of L30

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -4,17 +4,16 @@ import React from 'react';
 //  const video = props.video;
 //new object to pull video from props object
 //next line is ES6 version of above 2 lines.
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   console.log(video)
   const imageUrl = video.snippet.thumbnails.default.url;
   //get this tree from chrome > devtools > network > search > expand the tree
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src = {imageUrl} />
         </div>
-
         <div className="media-body">
           <div className="media-heading">{video.snippet.title}</div>
         </div>
